21406693d7f5d4bc0edfb802601fd63ee5ec3546
Fr3sssh <indraathalla@upi.edu>
2021-05-31 16:32:53 +0700

    Feat: Change Company Profile name

--- application/views/admin/va_users.php
+++ application/views/admin/va_users.php
@@ -261,7 +261,7 @@
                 <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid">
                         <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Our Last Destiny's 2021</div>
+                            <div class="text-muted">Copyright &copy; Our Last Destiny's Corporation 2021</div>
                             <div>
                                 <a href="#">Privacy Policy</a>
                                 &middot;
